4c949e731a67c39492ad58ec8c5b9d88c53f76fe
huangsen0912 <1417988198@qq.com>
2015-12-16 11:22:00 +0800

    db trace files

---
+++ basic/migrations/m151216_032117_create_core_brand.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m151216_032117_create_core_brand extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m151216_032117_create_core_brand cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
